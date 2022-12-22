f5a0bdf42aa434e2db268244712023d0767a9251
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-12-22 17:25:44 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 objects =[];
 status = "";
-
+var speech = Window.speechSynthesis;
 function setup(){
     canvas= createCanvas(500, 400);
     canvas.position(500, 300);
@@ -10,7 +10,7 @@ function setup(){
 
 function start() {
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Onjects";
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
   object_name =  document.getElementById("object_name").value;
    
 }
@@ -21,8 +21,46 @@ function modelLoaded() {
   
 }
 
+function gotResults(error, results) {
+  if(error){
+    console.log(error);
+  }
+  console.log(results);
+  objects = results;
+}
+
 function draw() {
     image(video, 0, 0, 500, 400);
+    if(status != "")
+    {
+        objectDetector.detect(video, gotResults);
+        for( i = 0; i < objects.length; i++ ){
+            document.getElementById("status").innerHTML = "Status : Detecting object";
 
-  }
+            if(objects[i].label == object_name){
+              fill("#00FF00");
+              percent = floor(objects[i].confidence * 100);
+              text(objects[i].label + " " + percent + "%", objects[i].x + 2, objects[i].y-15);
+              noFill();
+              stroke("#00FF00");
+              rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+  
+              document.getElementById("status").innerHTML = "Status : Object Detected";
+              video.stop();
+              objectDetector.detect(gotResults);
+              document.getElementById("number_of_objects").innerHTML =  object_name + " Detected";
+             utterthis = new  SpeechSynthesisUtterance(object_name + " found");
+             speech.speak(utterthis);
+            }
+            else{
+              document.getElementById("number_of_objects").innerHTML = object_name + " not detected";
+            }
+        }
+        
+
+       
+    }
+
+    
+ }
   
